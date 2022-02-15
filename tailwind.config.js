ad444a8c5d8e91b469a2f3ad5402bb7b1e9e8158
heokyoungmin <stalvede0421@gmail.com>
2022-02-15 19:26:44 +0900

    tailwind css install and setting

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}", // <- page에서 사용 할 수 있게 말을 해줘야함 page의 모든 폴더에 모든 파일. 확장자는 저뒤에
+    "./components/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
