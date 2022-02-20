278dc78927a7719bf2dc0f3cf2d37b9ebc0562ad
heokyoungmin <stalvede0421@gmail.com>
2022-02-20 23:58:08 +0900

    tailwind & list (array) view

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,28 @@ const Home: NextPage = () => {
     <div className="bg-slate-400 py-20 px-20 grid gap-10 min-h-screen">
       <div className="bg-white p-6 rounded-3xl shadow-xl">
         <span className="font-semibold text-2xl">Select Item</span>
-        <div className="flex justify-between my-2">
-          <span className="text-gray-500">Chair01</span>
-          <span className="font-semibold">$19</span>
-        </div>
-        <div className="flex justify-between">
-          <span className="text-gray-500">Chair02</span>
-          <span className="font-semibold">$19</span>
-        </div>
+        <ul>
+          {[1, 2, 3, 4, 5].map((item, key) => {
+            return (
+              <div
+                key={key}
+                className="flex justify-between my-2 odd:bg-blue-100 even:bg-yellow-100"
+              >
+                <span className="text-gray-500">Chair01</span>
+                <span className="font-semibold">$19</span>
+              </div>
+            );
+          })}
+        </ul>
+        <ul>
+          {["a", "b", "c", ""].map((item, key) => {
+            return (
+              <li className="bg-red-500 py-2 empty:hidden" key={key}>
+                {item}
+              </li>
+            );
+          })}
+        </ul>
         <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
           <span>Total</span>
           <span className="font-semibold">$10</span>
