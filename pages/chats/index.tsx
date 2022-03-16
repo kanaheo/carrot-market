bdaa1d66c190f88174bf0b2125232a3a4f2b7905
heokyoungmin <stalvede0421@gmail.com>
2022-03-17 00:00:17 +0900

    tailwind css practice end

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,21 +1,24 @@
 import type { NextPage } from "next";
+import Layout from "../../components/layout";
 
 const Chats: NextPage = () => {
   return (
-    <div className="py-10 divide-y-[1px]">
-      {new Array(6).fill(1).map((_, i) => (
-        <div
-          key={i}
-          className="flex px-4 cursor-pointer py-3 items-center space-x-3"
-        >
-          <div className="w-12 h-12 rounded-full bg-slate-300" />
-          <div>
-            <p className="text-gray-700">kkana</p>
-            <p className="text-sm text-gray-500">내일 2시에 보자잉</p>
+    <Layout hasTabBar title="채팅">
+      <div className="divide-y-[1px] ">
+        {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+          <div
+            key={i}
+            className="flex px-4 cursor-pointer py-3 items-center space-x-3"
+          >
+            <div className="w-12 h-12 rounded-full bg-slate-300" />
+            <div>
+              <p className="text-gray-700">까나</p>
+              <p className="text-sm  text-gray-500">내일 5시에 볼래?</p>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </Layout>
   );
 };
 
