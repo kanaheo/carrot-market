5f12f750ab53b7203b775e0f381a714b43afa903
heokyoungmin <stalvede0421@gmail.com>
2022-04-05 15:32:59 +0900

    twilio & sendgrid setting

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,6 +1,11 @@
+import mail from "@sendgrid/mail";
+import twilio from "twilio";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
+
+mail.setApiKey(process.env.SENDGRID_KEY!);
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 async function handler(
   req: NextApiRequest,
@@ -26,6 +31,23 @@ async function handler(
       },
     },
   });
+  if (phone) {
+    const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.MY_PHONE!,
+      body: `Your login token is ${payload}`,
+    });
+    console.log(message);
+  } else if (email) {
+    const email = await mail.send({
+      from: "stalvede7@naver.com", // 보내는 사람
+      to: "stalvede7@naver.com", // 받는사람
+      subject: "your kkana shop verification email",
+      text: `Your token is ${payload}`,
+      html: `<strong>Your token is ${payload}</strong>`,
+    });
+    console.log(email);
+  }
   return res.json({
     ok: true,
   });
