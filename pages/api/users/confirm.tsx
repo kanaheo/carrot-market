668c6cd4f998ea95f4e4e69ff4d9573d452430d2
heokyoungmin <stalvede0421@gmail.com>
2022-04-06 18:46:21 +0900

    iron-session setting

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,3 +1,4 @@
+import { withIronSessionApiRoute } from "iron-session/next";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
@@ -7,8 +8,21 @@ async function handler(
   res: NextApiResponse<ResponseType>
 ) {
   const { token } = req.body;
-  console.log(token);
+  const exists = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+  });
+  if (!exists) res.status(404).eventNames();
+  req.session.user = {
+    id: exists?.userId,
+  };
+  await req.session.save(); // session의 리턴값이 promise await는 promise가 끝날 때까지 기다리라고 지칭함
+
   res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrotsession",
+  password: "123545125123412431241zxcvzxcvzxvcsdfgasdfasdfasdfasdfasdf",
+});
