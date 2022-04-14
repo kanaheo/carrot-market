c8faa9b4777c3986284b08c12f3ced96517944cd
heokyoungmin <stalvede0421@gmail.com>
2022-04-14 22:30:41 +0900

    useSWR setting end

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,11 @@ import type { NextPage } from "next";
 import FloatingButton from "@components/floating-button";
 import Item from "@components/item";
 import Layout from "@components/layout";
+import useUser from "@libs/client/useUser";
 
 const Home: NextPage = () => {
+  const { user, isLoading } = useUser();
+  console.log(user);
   return (
     <Layout title="홈" hasTabBar>
       <div className="flex flex-col space-y-5 divide-y">
