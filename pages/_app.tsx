85985f02a76fe388b569190c22df1a99fa99b17b
heo_kyoungmin <heo_kyoungmin@fk.cocone.co.jp>
2022-05-19 15:02:08 +0900

    reset

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,8 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { SWRConfig } from "swr";
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <SWRConfig
-      value={{
-        fetcher: (url: string) =>
-          fetch(url).then((response) => response.json()),
-      }}
-    >
-      <div className="w-full max-w-xl mx-auto">
-        <Component {...pageProps} />
-      </div>
-    </SWRConfig>
-  );
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
