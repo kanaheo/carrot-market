1792b565cedb4c491f09a0143a8a7a51a8e06c63
heokyoungmin <stalvede0421@gmail.com>
2022-05-27 16:16:45 +0900

    chp8 end

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,5 +1,5 @@
+import withHandler from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
-import withHandler from "../../../libs/server/withHandler";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.body.email);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from "next";
-import FloatingButton from "../components/floating-button";
-import Item from "../components/item";
-import Layout from "../components/layout";
-import "../libs/server/client";
+import FloatingButton from "@components/floating-button";
+import Item from "@components/item";
+import Layout from "@components/layout";
+import "@libs/server/client";
 
 const Home: NextPage = () => {
   return (
