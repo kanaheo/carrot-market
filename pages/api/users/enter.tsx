3bc5f373db1c53ce69485305548caf618b5496b8
heokyoungmin <stalvede0421@gmail.com>
2022-05-30 00:10:59 +0900

    clean code

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,8 +1,64 @@
+import client from "@libs/server/client";
 import withHandler from "@libs/server/withHandler";
+import { prisma } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body.email);
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "kkana",
+      ...payload,
+    },
+    update: {},
+  });
+
+  console.log(user);
+
+  // if (phone) {
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       phone: +phone,
+  //     },
+  //   });
+  //   if (user) {
+  //     console.log("user is ! ");
+  //   }
+  //   if (!user) {
+  //     console.log("no user !! let's create user");
+  //     user = await client.user.create({
+  //       data: {
+  //         name: "kkana",
+  //         phone: +phone,
+  //       },
+  //     });
+  //   }
+  //   console.log(user);
+  // }
+  // if (email) {
+  //   user = await client.user.findUnique({
+  //     where: {
+  //       email,
+  //     },
+  //   });
+  //   if (user) {
+  //     console.log("user is ! ");
+  //   }
+  //   if (!user) {
+  //     console.log("no user !! let's create user");
+  //     user = await client.user.create({
+  //       data: {
+  //         name: "kkana",
+  //         email,
+  //       },
+  //     });
+  //   }
+  //   console.log(user);
+  // }
   return res.status(200).end();
 }
 
