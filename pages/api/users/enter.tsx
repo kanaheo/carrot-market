14e0e784c10f1b80c11139f2ec9214eb6dcf1b44
heokyoungmin <stalvede0421@gmail.com>
2022-05-30 00:36:57 +0900

    token model end

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -6,59 +6,27 @@ import { NextApiRequest, NextApiResponse } from "next";
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
   const payload = phone ? { phone: +phone } : { email };
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
+
+  const token = await client.token.create({
+    data: {
+      payload: "124124",
+      user: {
+        connectOrCreate: {
+          where: {
+            ...payload,
+          },
+          create: {
+            name: "kkana",
+            ...payload,
+          },
+        },
+      },
     },
-    create: {
-      name: "kkana",
-      ...payload,
-    },
-    update: {},
   });
 
-  console.log(user);
+  console.log("token");
+  console.log(token);
 
-  // if (phone) {
-  //   user = await client.user.findUnique({
-  //     where: {
-  //       phone: +phone,
-  //     },
-  //   });
-  //   if (user) {
-  //     console.log("user is ! ");
-  //   }
-  //   if (!user) {
-  //     console.log("no user !! let's create user");
-  //     user = await client.user.create({
-  //       data: {
-  //         name: "kkana",
-  //         phone: +phone,
-  //       },
-  //     });
-  //   }
-  //   console.log(user);
-  // }
-  // if (email) {
-  //   user = await client.user.findUnique({
-  //     where: {
-  //       email,
-  //     },
-  //   });
-  //   if (user) {
-  //     console.log("user is ! ");
-  //   }
-  //   if (!user) {
-  //     console.log("no user !! let's create user");
-  //     user = await client.user.create({
-  //       data: {
-  //         name: "kkana",
-  //         email,
-  //       },
-  //     });
-  //   }
-  //   console.log(user);
-  // }
   return res.status(200).end();
 }
 
