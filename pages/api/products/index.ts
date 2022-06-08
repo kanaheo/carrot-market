d64cf5aaacca3a03616cf9abfe813b739f5a62bc
heokyoungmin <stalvede0421@gmail.com>
2022-06-08 15:36:38 +0900

    ch 11 end

---
+++ pages/api/products/index.ts
@@ -0,0 +1,41 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    body: { name, price, description },
+    session: { user },
+  } = req;
+  const product = await client.product.create({
+    data: {
+      name,
+      price: +price,
+      description,
+      image: "xx",
+      user: {
+        connect: {
+          id: user?.id,
+        },
+      },
+    },
+  });
+
+  res.json({
+    ok: true,
+    product,
+  });
+
+  res.status(200).end();
+}
+
+export default withApiSession(
+  withHandler({
+    method: "POST",
+    handler,
+  })
+);
--- pages/products/upload.tsx
+++ pages/products/upload.tsx
@@ -6,6 +6,9 @@ import TextArea from "@components/textarea";
 import { useForm } from "react-hook-form";
 import { kill } from "process";
 import useMutation from "@libs/client/useMutation";
+import { useEffect } from "react";
+import { Product } from "@prisma/client";
+import { useRouter } from "next/router";
 
 interface UploadProductForm {
   name: string;
@@ -13,13 +16,25 @@ interface UploadProductForm {
   description: string;
 }
 
+interface UploadProductMutation {
+  ok: boolean;
+  product: Product;
+}
+
 const Upload: NextPage = () => {
+  const router = useRouter();
   const { register, handleSubmit } = useForm<UploadProductForm>();
-  const [uploadProduct, { loading, data }] = useMutation("/api/products");
+  const [uploadProduct, { loading, data }] =
+    useMutation<UploadProductMutation>("/api/products");
   const onValid = (data: UploadProductForm) => {
     if (loading) return;
     uploadProduct(data);
   };
+  useEffect(() => {
+    if (data?.ok) {
+      router.push(`/products/${data.product.id}`);
+    }
+  }, [data, router]);
   return (
     <Layout canGoBack title="Upload Product">
       <form className="p-4 space-y-4" onSubmit={handleSubmit(onValid)}>
