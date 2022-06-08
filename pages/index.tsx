545fe90924346e391d3f40089f7d49724ce862ce
heokyoungmin <stalvede0421@gmail.com>
2022-06-08 12:18:32 +0900

    products db create

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,10 @@ import type { NextPage } from "next";
 import FloatingButton from "@components/floating-button";
 import Item from "@components/item";
 import Layout from "@components/layout";
-import useUser from "@libs/client/useUser";
-import Head from "next/head";
 
 const Home: NextPage = () => {
-  const { user, isLoading } = useUser();
   return (
     <Layout title="홈" hasTabBar>
-      <Head>
-        <title>Home</title>
-      </Head>
       <div className="flex flex-col space-y-5 divide-y">
         {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
           <Item
