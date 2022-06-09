21ce2aa61f20805e7469a3d9a29f24c54f4b7293
heokyoungmin <stalvede0421@gmail.com>
2022-06-09 14:55:44 +0900

    Optimistic UI Update

---
+++ pages/api/products/[id]/fav.ts
@@ -0,0 +1,52 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+    session: { user },
+  } = req;
+  const alreadyExists = await client.fav.findFirst({
+    where: {
+      productId: +id.toString(),
+      userId: user?.id,
+    },
+  });
+  if (alreadyExists) {
+    // delete
+    await client.fav.delete({
+      where: {
+        id: alreadyExists.id,
+      },
+    });
+  } else {
+    // add
+    await client.fav.create({
+      data: {
+        user: {
+          connect: {
+            id: user?.id,
+          },
+        },
+        product: {
+          connect: {
+            id: +id.toString(),
+          },
+        },
+      },
+    });
+  }
+  res.json({ ok: true });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["POST"],
+    handler,
+  })
+);
