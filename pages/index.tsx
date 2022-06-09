788ae7ce9b8aa7ff0fb136c83fceb6825ad6eb6d
heokyoungmin <stalvede0421@gmail.com>
2022-06-09 15:15:02 +0900

    chapter 11 end

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,15 @@ import useUser from "@libs/client/useUser";
 import useSWR from "swr";
 import { Product } from "@prisma/client";
 
+interface ProductWithCount extends Product {
+  _count: {
+    favs: number;
+  };
+}
+
 interface ProductsResponse {
   ok: boolean;
-  products: Product[];
+  products: ProductWithCount[];
 }
 
 const Home: NextPage = () => {
@@ -24,7 +30,7 @@ const Home: NextPage = () => {
             title={product.name}
             price={product.price}
             comments={1}
-            hearts={1}
+            hearts={product._count.favs}
           />
         ))}
         <FloatingButton href="/products/upload">
