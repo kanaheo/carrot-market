5fc1be8e0e329cd4069c986ad0e814a38e2d3e48
heo_kyoungmin <heo_kyoungmin@fk.cocone.co.jp>
2022-06-13 12:24:43 +0900

    community write end

---
+++ pages/api/posts/index.ts
@@ -0,0 +1,39 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    body: { question },
+    session: { user },
+  } = req;
+
+  const post = await client.post.create({
+    data: {
+      question,
+      user: {
+        connect: {
+          id: user?.id,
+        },
+      },
+    },
+  });
+
+  res.json({
+    ok: true,
+    post,
+  });
+
+  res.status(200).end();
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["POST"],
+    handler,
+  })
+);
