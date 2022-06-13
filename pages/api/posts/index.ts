39bd84029d73e448799542b608663177bd51d541
heo_kyoungmin <heo_kyoungmin@fk.cocone.co.jp>
2022-06-13 17:20:06 +0900

    latitude & longitude add

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -8,7 +8,7 @@ async function handler(
   res: NextApiResponse<ResponseType>
 ) {
   const {
-    body: { question },
+    body: { question, latitude, longitude },
     session: { user },
   } = req;
 
@@ -16,6 +16,8 @@ async function handler(
     const post = await client.post.create({
       data: {
         question,
+        latitude,
+        longitude,
         user: {
           connect: {
             id: user?.id,
--- pages/community/write.tsx
+++ pages/community/write.tsx
@@ -8,6 +8,7 @@ import { useEffect } from "react";
 import { BundleCopyListInstanceEachOptions } from "twilio/lib/rest/numbers/v2/regulatoryCompliance/bundle/bundleCopy";
 import { Post } from "@prisma/client";
 import { useRouter } from "next/router";
+import useCoords from "@libs/client/useCoords";
 
 interface WriteForm {
   question: string;
@@ -19,12 +20,13 @@ interface WriteResponse {
 }
 
 const Write: NextPage = () => {
+  const { latitude, longitude } = useCoords();
   const router = useRouter();
   const { register, handleSubmit } = useForm<WriteForm>();
   const [post, { loading, data }] = useMutation<WriteResponse>("/api/posts");
   const onValid = (data: WriteForm) => {
     if (loading) return;
-    post(data);
+    post({ ...data, latitude, longitude });
   };
   useEffect(() => {
     if (data && data.ok) {
