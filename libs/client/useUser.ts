4411175e9fb8a44f9c13cb19cf435fedfbf805e4
heokyoungmin <stalvede0421@gmail.com>
2022-06-17 16:48:12 +0900

    myprofile edit page editing

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -2,9 +2,14 @@ import { Router, useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import useSWR from "swr";
 
+interface ProfileResponse {
+  ok: boolean;
+  profile: User;
+}
+
 export default function useUser() {
   // /api/users/me key와 같은 역할도 한다 ! 이것을 그대로 다른 컴포넌트에서 쓰면 사용 가능하다 !!! 저대로!! 굳이 리덕스를 안써도 될 듯
-  const { data, error } = useSWR("/api/users/me");
+  const { data, error } = useSWR<ProfileResponse>("/api/users/me");
   const router = useRouter();
   useEffect(() => {
     if (data && !data.ok) {
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 import { SWRConfig } from "swr";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // TODO 여기서 useUser불러오기 ! 대신 enter페이지는 안된다 !
   return (
     <SWRConfig
       value={{
