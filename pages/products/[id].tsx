ef2a9ad695a2e532a3f4e0ad90cebdabb6b01454
heo_kyoungmin <heo_kyoungmin@fk.cocone.co.jp>
2022-07-05 17:57:19 +0900

    profile end

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -42,9 +42,15 @@ const ItemDetail: NextPage = () => {
     <Layout canGoBack>
       <div className="px-4  py-4">
         <div className="mb-8">
-          <div className="h-96 bg-slate-300" />
+          <img
+            src={`https://imagedelivery.net/7VbZB4oVIlSndB1f5dV2Pw/${data?.product.image}/public`}
+            className="h-96 bg-slate-300"
+          />
           <div className="flex cursor-pointer py-3 border-t border-b items-center space-x-3">
-            <div className="w-12 h-12 rounded-full bg-slate-300" />
+            <img
+              src={`https://imagedelivery.net/7VbZB4oVIlSndB1f5dV2Pw/${data?.product?.user?.avatar}/avatar`}
+              className="w-12 h-12 rounded-full bg-slate-300"
+            />
             <div>
               <p className="text-sm font-medium text-gray-700">
                 {data?.product?.user?.name}
--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -23,7 +23,7 @@ const Profile: NextPage = () => {
         <div className="flex items-center mt-4 space-x-3">
           {user?.avatar ? (
             <img
-              src={`https://imagedelivery.net/7VbZB4oVIlSndB1f5dV2Pw/${user?.avatar}/avatar`}
+              src={`https://imagedelivery.net/7VbZB4oVIlSndB1f5dV2Pw/${user?.avatar}/public`}
               className="w-16 h-16 bg-slate-500 rounded-full"
             />
           ) : (
