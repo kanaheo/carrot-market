3f6a8969fd7bd5160ec24867e457ceb363d3d15d
heo_kyoungmin <heo_kyoungmin@fk.cocone.co.jp>
2022-07-06 18:02:57 +0900

    chp16 end

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,6 @@ const Home: NextPage = () => {
             key={product.id}
             title={product.name}
             price={product.price}
-            comments={1}
             hearts={product._count.favs}
           />
         ))}
--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -8,11 +8,11 @@ import { Product, User } from "@prisma/client";
 import useMutation from "@libs/client/useMutation";
 import { cls } from "@libs/client/utils";
 import useUser from "@libs/client/useUser";
+import Image from "next/image";
 
 interface ProductWithUser extends Product {
   user: User;
 }
-
 interface ItemDetailResponse {
   ok: boolean;
   product: ProductWithUser;
@@ -27,30 +27,41 @@ const ItemDetail: NextPage = () => {
   const { data, mutate: boundMutate } = useSWR<ItemDetailResponse>(
     router.query.id ? `/api/products/${router.query.id}` : null
   );
-  const [toggleFav, { loading }] = useMutation(
-    `/api/products/${router.query.id}/fav`
-  );
+  const [toggleFav] = useMutation(`/api/products/${router.query.id}/fav`);
   const onFavClick = () => {
     if (!data) return;
     boundMutate((prev) => prev && { ...prev, isLiked: !prev.isLiked }, false);
     // mutate("/api/users/me", (prev: any) => ({ ok: !prev.ok }), false);
-    if (!loading) {
-      toggleFav({});
-    }
+    toggleFav({});
   };
   return (
     <Layout canGoBack>
       <div className="px-4  py-4">
         <div className="mb-8">
-          <img
-            src={`https://imagedelivery.net/7VbZB4oVIlSndB1f5dV2Pw/${data?.product.image}/public`}
-            className="h-96 bg-slate-300"
-          />
+          <div className="relative pb-80">
+            {data?.product ? (
+              <Image
+                src={`https://imagedelivery.net/7VbZB4oVIlSndB1f5dV2Pw/${data?.product?.image}/public`}
+                className="bg-slate-300 object-corver"
+                layout="fill"
+              />
+            ) : (
+              ""
+            )}
+          </div>
           <div className="flex cursor-pointer py-3 border-t border-b items-center space-x-3">
-            <img
-              src={`https://imagedelivery.net/7VbZB4oVIlSndB1f5dV2Pw/${data?.product?.user?.avatar}/avatar`}
-              className="w-12 h-12 rounded-full bg-slate-300"
-            />
+            {data?.product?.user ? (
+              <Image
+                width={48}
+                height={48}
+                src={`https://imagedelivery.net/7VbZB4oVIlSndB1f5dV2Pw/${data?.product?.user?.avatar}/avatar`}
+                className="w-12 h-12 rounded-full bg-slate-300"
+                alt="fjajsdf"
+              />
+            ) : (
+              ""
+            )}
+
             <div>
               <p className="text-sm font-medium text-gray-700">
                 {data?.product?.user?.name}
@@ -77,8 +88,8 @@ const ItemDetail: NextPage = () => {
                 className={cls(
                   "p-3 rounded-md flex items-center hover:bg-gray-100 justify-center ",
                   data?.isLiked
-                    ? "text-red-500 hover:text-red-600"
-                    : "text-gray-400 hover:text-gray-500"
+                    ? "text-red-500  hover:text-red-600"
+                    : "text-gray-400  hover:text-gray-500"
                 )}
               >
                 {data?.isLiked ? (
@@ -92,7 +103,7 @@ const ItemDetail: NextPage = () => {
                       fillRule="evenodd"
                       d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
                       clipRule="evenodd"
-                    />
+                    ></path>
                   </svg>
                 ) : (
                   <svg
@@ -118,18 +129,14 @@ const ItemDetail: NextPage = () => {
         <div>
           <h2 className="text-2xl font-bold text-gray-900">Similar items</h2>
           <div className=" mt-6 grid grid-cols-2 gap-4">
-            {data?.relatedProducts.map((product) => (
-              <Link href={`/products/${product.id}`} key={product.id}>
-                <a>
-                  <div>
-                    <div className="h-56 w-full mb-4 bg-slate-300" />
-                    <h3 className="text-gray-700 -mb-1">{product.name}</h3>
-                    <span className="text-sm font-medium text-gray-900">
-                      ${product.price}
-                    </span>
-                  </div>
-                </a>
-              </Link>
+            {data?.relatedProducts?.map((product) => (
+              <div key={product.id}>
+                <div className="h-56 w-full mb-4 bg-slate-300" />
+                <h3 className="text-gray-700 -mb-1">{product.name}</h3>
+                <span className="text-sm font-medium text-gray-900">
+                  ${product.price}
+                </span>
+              </div>
             ))}
           </div>
         </div>
