d35e782d54349839a0f5cb38549292070c08648e
heo_kyoungmin <heo_kyoungmin@fk.cocone.co.jp>
2022-07-20 15:51:34 +0900

    reservation editing

--- pages/chats/[id]/index.tsx
+++ pages/chats/[id]/index.tsx
@@ -47,7 +47,7 @@ const ChatDetail: NextPage = () => {
   }, [chatData, reset, mutate]);
 
   return (
-    <Layout canGoBack title="Steve">
+    <Layout canGoBack title={user?.name}>
       <div className="py-10 pb-16 px-4 space-y-4">
         {data &&
           user &&
@@ -64,19 +64,28 @@ const ChatDetail: NextPage = () => {
           className="fixed py-2 bg-white  bottom-0 inset-x-0"
           onSubmit={handleSubmit(onValid)}
         >
-          <div className="flex relative max-w-md items-center  w-full mx-auto">
+          <div className="flex relative max-w-md items-center w-full mx-auto">
             <input
               {...register("chat")}
               className="shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-500 focus:outline-none pr-12 focus:border-orange-500"
               type="text"
             />
-            <div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
+            <div className="absolute inset-y-0 flex right-0">
               <button className="flex focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 items-center bg-orange-500 rounded-full px-3 hover:bg-orange-600 text-sm text-white">
                 &rarr;
               </button>
             </div>
           </div>
         </form>
+        <div className="fixed py-2 bottom-6 inset-x-0">
+          <div className="flex relative max-w-md items-center w-full mx-auto">
+            <div className="absolute inset-y-0 flex -right-32 h-6">
+              <button className="flex focus:ring-2 focus:ring-offset-2 focus:ring-lime-600 items-center bg-lime-600 rounded-full px-3 hover:bg-lime-700 text-sm text-white">
+                예약
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </Layout>
   );
