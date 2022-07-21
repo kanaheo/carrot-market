957147c9e6481955f5093a40c816eacd50340bf2
heo_kyoungmin <heo_kyoungmin@fk.cocone.co.jp>
2022-07-21 18:36:17 +0900

    middleware

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,3 +1,15 @@
-export function middleware() {
-  console.log("it works???");
+import { NextRequest, NextFetchEvent, NextResponse } from "next/server";
+
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+  if (req.ua?.isBot) {
+    return new Response("Plz don't a Bot.Be human.", { status: 403 });
+  }
+
+  console.log(req.url);
+
+  if (!req.url.includes("/api")) {
+    if (!req.url.includes("/enter") && !req.cookies.carrotsession) {
+      return NextResponse.redirect("/enter");
+    }
+  }
 }
