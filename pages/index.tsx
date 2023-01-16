8eff0c950a51c3042718ffcea3f2ef6821f76511
heo_kyoungmin <heo_kyoungmin@fk.cocone.co.jp>
2023-01-16 15:19:11 +0900

    default setting end

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>asdfasdf</div>;
+  return (
+    <div className="bg-red-500">
+      <h1 className="text-black">asdfasdf</h1>
+    </div>
+  );
 };
 
 export default Home;
