3a9d706677646181d1d930bc342458029d88c5c7
k-heo <stalvede7@naver.com>
2023-05-16 17:23:48 +0900

    init setting

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["imagedelivery.net"],
-  },
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
