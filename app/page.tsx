6403dfdcaa5bcf167b41dfb6229317b50e671761
kanaheo <stalvede0421@gmail.com>
2023-05-18 09:31:12 +0900

    tailwindcss practice and preitter setting

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 export default function Home() {
+  const jony = {
+    name: "jfaksjdfkj",
+  };
   return (
     <div className="bg-slate-400 py-20 px-20 grid gap-10 min-h-screen">
       <div className="bg-white p-6 rounded-3xl shadow-xl">
-        <span className="font-semibold text-3xl">Select Item</span>
+        <span className="font-semibold text-2xl">Select Item</span>
         <div className="flex justify-between my-2">
           <span className="text-gray-500">Grey Chair</span>
           <span className="font-semibold">$19</span>
@@ -15,13 +18,14 @@ export default function Home() {
           <span>Total</span>
           <span className="font-semibold">$10</span>
         </div>
-        <div
+        <button
           className="mt-5 bg-blue-500 text-white p-3
-          text-center rounded-xl w-2/4 mx-auto
-         "
+          text-center rounded-xl w-3/4 mx-auto 
+          hover:bg-teal-500 hover:text-black
+          active:bg-yellow-500 focus:bg-red-500"
         >
           Checkout
-        </div>
+        </button>
       </div>
       <div className="bg-white overflow-hidden rounded-3xl shadow-xl">
         <div className="bg-blue-500 p-6 pb-14">
@@ -30,18 +34,18 @@ export default function Home() {
         <div className="rounded-3xl p-6 bg-white relative -top-5">
           <div className="flex relative -top-16 items-end justify-between">
             <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-500">Orders</span>
+              <span className="text-xs text-gray-500">Orders</span>
               <span className="font-medium">340</span>
             </div>
-            <div className="h-24 w-24 bg-red-400 rounded-full" />
+            <div className="h-24 w-24 bg-zinc-300 rounded-full" />
             <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-500">Spent</span>
+              <span className="text-xs text-gray-500">Spent</span>
               <span className="font-medium">$340</span>
             </div>
           </div>
-          <div className="relative  flex flex-col items-center -mt-10 -mb-5">
+          <div className="relative  flex flex-col items-center -mt-14 -mb-5">
             <span className="text-lg font-medium">KKana</span>
-            <span className="text-sm text-gray-500">미국</span>
+            <span className="text-sm text-gray-500">KR</span>
           </div>
         </div>
       </div>
@@ -58,10 +62,10 @@ export default function Home() {
           <span className="font-medium text-xl">Swoon Lounge</span>
           <span className="text-xs text-gray-500">Chair</span>
           <div className="mt-3 mb-5 flex justify-between items-center">
-            <div>
-              <input type="radio" />
-              <input type="radio" />
-              <input type="radio" />
+            <div className="space-x-2">
+              <button className="w-5 h-5 rounded-full bg-yellow-500" />
+              <button className="w-5 h-5 rounded-full bg-indigo-500" />
+              <button className="w-5 h-5 rounded-full bg-teal-500" />
             </div>
             <div className="flex items-center space-x-5">
               <button className=" rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500">
@@ -81,7 +85,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="bg-white p-6 rounded-3xl shadow-xl"></div>
     </div>
-  )
+  );
 }
