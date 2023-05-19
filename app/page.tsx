c79d31eb291a943200847834a4635407ddd3e9af
kanaheo <stalvede0421@gmail.com>
2023-05-19 13:19:51 +0900

    responsive modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,11 @@ export default function Home() {
     name: "jfaksjdfkj",
   };
   return (
-    <form className="flex flex-col space-y-2  p-5 ">
+    <div className="flex flex-col space-y-2 p-5">
       <input
-        type="text"
-        required
-        placeholder="Username"
-        className="border p-1 peer border-gray-400 rounded-md "
+        type="file"
+        className="file:cursor-pointer transition-colors file:border-0 file:rounded-xl file:px-5 file:text-white file:bg-purple-400"
       />
-      <span className="hidden peer-invalid:block peer-invalid:text-red-500">
-        This input is invalid
-      </span>
-      <span className="hidden peer-valid:block peer-valid:text-teal-500">Awesome username</span>
-      <span className="hidden peer-hover:block peer-hover:text-amber-500">Hello</span>
-      <input type="submit" value="Login" className="bg-white" />
-    </form>
+    </div>
   );
 }
--- app/tailwindcss_example/page.tsx
+++ app/tailwindcss_example/page.tsx
@@ -2,11 +2,14 @@ export default function Home() {
   // hover, group 등등 연습
   return (
     <div className="bg-slate-400 py-20 px-20 grid gap-10 min-h-screen">
-      <div className="bg-white p-6 rounded-3xl shadow-xl">
+      <div className="bg-white sm:bg-red-400 md:bg-teal-400 lg:bg-indigo-400 xl:bg-yellow-400 2xl:bg-pink-500 p-6 rounded-3xl shadow-xl">
         <span className="font-semibold text-2xl">Select Item</span>
         <ul>
           {[1, 2, 3, 4, 5].map((i) => (
-            <div key={i} className="flex justify-between my-2 odd:bg-blue-50 even:bg-yellow-500">
+            <div
+              key={i}
+              className="flex justify-between my-2 odd:bg-blue-50 even:bg-yellow-500"
+            >
               <span className="text-gray-500">Grey Chair</span>
               <span className="font-semibold">$19</span>
             </div>
