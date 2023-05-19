81af88b65133c1fb26f848ebf76703a12baa18a6
kanaheo <stalvede0421@gmail.com>
2023-05-19 14:08:12 +0900

    dark mode

--- app/tailwindcss_example/page.tsx
+++ app/tailwindcss_example/page.tsx
@@ -1,17 +1,22 @@
 export default function Home() {
   // hover, group 등등 연습
   return (
+    // <div className="bg-slate-400 dark xl:place-content-center py-20 px-20 grid gap-10 lg:grid-cols-2 xl:grid-cols-3 min-h-screen">
     <div className="bg-slate-400 xl:place-content-center py-20 px-20 grid gap-10 lg:grid-cols-2 xl:grid-cols-3 min-h-screen">
-      <div className="bg-white flex flex-col sm:bg-red-400 md:bg-teal-400 lg:bg-indigo-400 xl:bg-yellow-400 2xl:bg-pink-500 p-6 rounded-3xl shadow-xl">
-        <span className="font-semibold text-2xl">Select Item</span>
+      <div className="bg-white dark:bg-black flex flex-col sm:bg-red-400 md:bg-teal-400 lg:bg-indigo-400 xl:bg-yellow-400 2xl:bg-pink-500 p-6 rounded-3xl shadow-xl">
+        <span className="font-semibold dark:text-white text-2xl">
+          Select Item
+        </span>
         <ul>
           {[1, 2, 3, 4, 5].map((i) => (
             <div
               key={i}
-              className="flex justify-between my-2 odd:bg-blue-50 even:bg-yellow-500"
+              className="flex justify-between my-2 dark:bg-black bg-blue-50"
             >
-              <span className="text-gray-500">Grey Chair</span>
-              <span className="font-semibold">$19</span>
+              <span className="text-gray-500 dark:text-gray-100">
+                Grey Chair
+              </span>
+              <span className="font-semibold dark:text-white">$19</span>
             </div>
           ))}
         </ul>
@@ -20,10 +25,11 @@ export default function Home() {
           <span className="font-semibold">$10</span>
         </div>
         <button
-          className="mt-5 bg-blue-500 text-white p-3
-          text-center rounded-xl w-3/4 mx-auto 
-          hover:bg-teal-500 hover:text-black
-          active:bg-yellow-500 focus:bg-red-500"
+          className="mt-5 bg-blue-500 dark:bg-black dark:border-white dark:border text-white p-3
+          text-center rounded-xl w-3/4 block mx-auto 
+          dark:hover:bg-white hover:bg-teal-500 hover:text-black 
+          active:bg-yellow-500 focus:bg-red-500
+          "
         >
           Checkout
         </button>
