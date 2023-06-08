e528d2b1d1afba1e3c258c9a614753140822e28c
kanaheo <stalvede0421@gmail.com>
2023-06-08 14:40:57 +0900

    layout and enter page editting

--- app/enter/page.tsx
+++ app/enter/page.tsx
@@ -2,10 +2,7 @@
 
 import type { NextPage } from "next";
 import { useState } from "react";
-
-function cls(...classnames: string[]) {
-  return classnames.join(" ");
-}
+import { cls } from "../libs/utils";
 
 const Enter: NextPage = () => {
   const [method, setMethod] = useState<"email" | "phone">("email");
