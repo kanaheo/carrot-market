e66bdf8d9c43e5483a9a3db5e317268da6518bc9
kanaheo <stalvede0421@gmail.com>
2023-06-27 15:17:50 +0900

    api post practice for next js 13

---
+++ app/api/users/enter/route.tsx
@@ -0,0 +1,12 @@
+import client from "@/app/libs/client";
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET(req: NextRequest, res: NextResponse) {
+  return NextResponse.json({ ok: true });
+}
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  const data = await req.json();
+  console.log(data.email);
+  return NextResponse.json({ ok: true });
+}
