6a353cae77ed4373ba9117b85e21d96be60efcd9
kanaheo <stalvede0421@gmail.com>
2023-09-15 01:06:01 +0900

    api function add handler

--- app/api/users/enter/route.tsx
+++ app/api/users/enter/route.tsx
@@ -2,11 +2,22 @@ import client from "@/app/libs/server/client";
 import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(req: NextRequest, res: NextResponse) {
-  return NextResponse.json({ ok: true });
+  try {
+    // 연습삼아 500에러를 일부로 ~
+    const data = await req.json();
+    return NextResponse.json({ ok: true });
+  } catch (err) {
+    return new NextResponse("Error content", { status: 500 });
+  }
 }
 
 export async function POST(req: NextRequest, res: NextResponse) {
-  const data = await req.json();
-  console.log(data);
-  return NextResponse.json({ ok: true });
+  try {
+    const data = await req.json();
+    console.log(data);
+    return NextResponse.json({ ok: true });
+  } catch (error) {
+    // 개발자 툴 보면 status정보나옴
+    return new NextResponse("Error content", { status: 500 });
+  }
 }
