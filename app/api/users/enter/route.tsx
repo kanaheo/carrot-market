59cb648160c74b62f198110aa579799804594c9d
kanaheo <stalvede0421@gmail.com>
2023-09-19 16:54:37 +0900

    sms and email send test

--- app/api/users/enter/route.tsx
+++ app/api/users/enter/route.tsx
@@ -1,5 +1,11 @@
+import mail from "@sendgrid/mail";
+import twilio from "twilio";
 import client from "@/libs/server/client";
 import { NextResponse, NextRequest } from "next/server";
+
+// email sms test~
+// mail.setApiKey(process.env.SENDGRID_KEY!);
+// const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
 export async function GET(req: NextRequest, res: NextResponse) {
   try {
@@ -18,25 +24,46 @@ export async function GET(req: NextRequest, res: NextResponse) {
 export async function POST(req: NextRequest, res: NextResponse) {
   try {
     const { email, phone } = await req.json();
-    const payload = phone ? { phone: +phone } : { email };
+    const user = phone ? { phone: +phone } : email ? { email } : "";
+    if (!user)
+      return new NextResponse("Error content", {
+        status: 400,
+      });
+    const payload = Math.floor(100000 + Math.random() * 900000) + "";
     const token = await client.token.create({
       data: {
-        payload: "1234121",
+        payload,
         user: {
           connectOrCreate: {
             where: {
-              ...payload,
+              ...user,
             },
             create: {
               name: "kknkkm",
-              ...payload,
+              ...user,
             },
           },
         },
       },
     });
 
-    console.log(token);
+    // if (phone) {
+    //   const message = await twilioClient.messages.create({
+    //     messagingServiceSid: process.env.TWILIO_MSID,
+    //     // to: phone 원래는 이런 이름으로 !!! phone를 적어서 보낼 사람에게 보내야함 !
+    //     to: process.env.MY_PHONE!,
+    //     body: `Your Login token is ${payload}`,
+    //   });
+    // }
+    // if (email) {
+    //   const email = await mail.send({
+    //     from: "stalvede0421@gmail.com",
+    //     to: "stalvede0421@gmail.com",
+    //     subject: "Your Carrot Market Verification Email",
+    //     text: `Your token is ${payload}`,
+    //     html: `<strong>Your token is ${payload}</strong>`,
+    //   });
+    // }
 
     return NextResponse.json({ ok: true });
   } catch (error) {
